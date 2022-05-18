de34e8008a47fa846fa71459fdedb88e9159412c
Tudor Nazarie <tudor@nazarie.net>
2022-05-18 04:25:30 +0100

    Tweak conditional shapes

--- cyberbattle/agents/sb3/ppo_cat/distributions.py
+++ cyberbattle/agents/sb3/ppo_cat/distributions.py
@@ -53,48 +53,74 @@ class VATMultiCategoricalDistribution(VATDistribution):
 
     def log_prob(self, actions: th.Tensor) -> Tuple[th.Tensor, th.Tensor]:
         assert len(self.distributions) > 0, "Must set distribution parameters"
-        action = actions[0]
-        subtree = self.valid_action_trees[0][0]
-        action_type, source_node, target_node, local_vuln, remote_vuln, port, cred = range(7)
 
-        logp_parts = th.zeros([7])
-        entropy_parts = th.zeros([7])
+        logp = th.zeros([self.valid_action_trees.shape[0]])
+        entropy = th.zeros([self.valid_action_trees.shape[0]])
+        split_dists = [VATMultiCategoricalDistribution._split_categorical(d) for d in self.distributions]
+        # transpose it
+        split_dists = list(map(list, zip(*split_dists)))
+        for idx, (action, vat, dists) in enumerate(zip(actions, self.valid_action_trees, split_dists)):
+            action_logp, action_entropy = VATMultiCategoricalDistribution._logp_and_entropy_for_action(action, vat,
+                                                                                                       dists)
+            logp[idx] = action_logp.sum()
+            entropy[idx] = action_logp.sum()
 
-        sampled_type = action[action_type]
-        subtree = subtree[int(sampled_type)]
-        if sampled_type == 0:
-            for part in [source_node, local_vuln]:
-                logp, entropy = self._logp_and_entropy_for_action_part(subtree, action[part], part)
-                logp_parts[part] = logp
-                entropy_parts[part] = entropy
-                subtree = subtree[int(action[part])]
-        elif sampled_type == 1:
-            for part in [source_node, target_node, remote_vuln]:
-                logp, entropy = self._logp_and_entropy_for_action_part(subtree, action[part], part)
-                logp_parts[part] = logp
-                entropy_parts[part] = entropy
-                subtree = subtree[int(action[part])]
-        else:
-            for part in [source_node, port, cred]:
-                logp, entropy = self._logp_and_entropy_for_action_part(subtree, action[part], part)
-                logp_parts[part] = logp
-                entropy_parts[part] = entropy
-                subtree = subtree[int(action[part])]
-        return logp_parts.sum(), entropy_parts.sum()
-
-    def _logp_and_entropy_for_action_part(self, subtree: Dict, sampled: th.Tensor, dist_pos: int) -> Tuple[th.Tensor, th.Tensor]:
-        options = list(subtree.keys())
-        mask = np.full_like(self.distributions[dist_pos].logits[0], False, dtype=bool)
-        mask[options] = True
-        self.distributions[dist_pos].apply_masking(mask)
-
-        logp = self.distributions[dist_pos].log_prob(sampled)
-        entropy = self.distributions[dist_pos].entropy()
         return logp, entropy
 
+    @staticmethod
+    def _logp_and_entropy_for_action(action: th.Tensor, vat: np.ndarray, dists: List[MaskableCategorical]):
+        assert isinstance(vat[0], Dict)
+        logp = th.zeros([len(action)])
+        entropy = th.zeros([len(action)])
+
+        subtree = vat[0]
+        action_type, source_node, target_node, local_vuln, remote_vuln, port, cred = range(7)
+        sel_action = action[action_type]
+        options = list(subtree.keys())
+        # mask = np.full_like(dists[action_type].logits, False, dtype=bool)
+        mask = np.full(tuple(dists[action_type].logits.shape), False, dtype=bool)
+        mask[options] = True
+        dists[action_type].apply_masking(mask)
+
+        sel_action_logp = dists[action_type].log_prob(sel_action)
+        logp[action_type] = sel_action_logp
+        entropy[action_type] = dists[action_type].entropy()
+
+        subtree = subtree[int(sel_action)]
+        other_parts = {
+            0: [source_node, local_vuln],
+            1: [source_node, target_node, remote_vuln],
+            2: [source_node, port, cred],
+        }
+        for part in other_parts[int(sel_action)]:
+            options = list(subtree.keys())
+            selection = action[part]
+            # mask = np.full_like(dists[part].logits, False, dtype=bool)
+            mask = np.full(tuple(dists[part].logits.shape), False, dtype=bool)
+            mask[options] = True
+            dists[part].apply_masking(mask)
+
+            lp = dists[part].log_prob(selection)
+            logp[part] = lp
+            entropy[part] = dists[part].entropy()
+
+            subtree = subtree[int(selection)]
+
+        return logp, entropy
+
+    # def _logp_and_entropy_for_action_part(self, subtree: Dict, sampled: th.Tensor, dist: MaskableCategorical) -> Tuple[th.Tensor, th.Tensor]:
+    #     options = list(subtree.keys())
+    #     mask = np.full_like(dist.logits[0], False, dtype=bool)
+    #     mask[options] = True
+    #     self.distributions[dist_pos].apply_masking(mask)
+    #
+    #     logp = self.distributions[dist_pos].log_prob(sampled)
+    #     entropy = self.distributions[dist_pos].entropy()
+    #     return logp, entropy
+
     def entropy(self) -> th.Tensor:
-        assert len(self.distributions) > 0, "Must set distribution parameters"
-        return th.stack([dist.entropy() for dist in self.distributions], dim=1).sum(dim=1)
+        # need an action for that one
+        raise NotImplementedError
 
     def sample(self) -> th.Tensor:
         assert len(self.distributions) > 0, "Must set distribution parameters"
@@ -117,218 +143,73 @@ class VATMultiCategoricalDistribution(VATDistribution):
     def apply_vat(self, vat: Optional[np.ndarray]) -> None:
         self.valid_action_trees = vat
 
-    def _mask_and_sample(self, deterministic: bool = False) -> th.Tensor:
+    def _mask_and_sample(self, deterministic=False) -> th.Tensor:
         assert self.valid_action_trees is not None
-        assert isinstance(self.valid_action_trees[0][0], Dict)
 
-        subtree = self.valid_action_trees[0][0]
-        action = th.zeros([7], dtype=th.int)
+        actions = th.zeros([self.valid_action_trees.shape[0], len(self.action_dims)], dtype=th.int)
+        logp = th.zeros([len(self.action_dims)])
+        split_dists = [VATMultiCategoricalDistribution._split_categorical(d) for d in self.distributions]
+        # transpose it
+        split_dists = list(map(list, zip(*split_dists)))
+
+        for idx, (vat, dists) in enumerate(zip(self.valid_action_trees, split_dists)):
+            assert isinstance(vat[0], Dict)
+            action, action_logp = self._mask_and_sample_full_action(dists, vat, deterministic)
+            actions[idx] = action
+            logp[idx] = action_logp
+
+        return actions
+
+    def _mask_and_sample_full_action(self, dists: List[MaskableCategorical], vat: np.ndarray,
+                                     deterministic: bool) -> Tuple[th.Tensor, th.Tensor]:
+        assert isinstance(vat[0], Dict)
+
+        action = th.zeros(len(self.action_dims), dtype=th.int)
+        logp = th.zeros(len(self.action_dims))
+        subtree: Dict = vat[0]
         action_type, source_node, target_node, local_vuln, remote_vuln, port, cred = range(7)
-        sampled_type = self._mask_and_sample_action_part(subtree, action_type, deterministic)
+        sampled_type, sampled_type_logp, _ = self._mask_and_sample_action_part(dists[action_type], subtree,
+                                                                               deterministic)
         action[action_type] = sampled_type
+        logp[action_type] = sampled_type_logp
+
         subtree = subtree[int(sampled_type)]
+        other_action_parts = {
+            0: [source_node, local_vuln],
+            1: [source_node, target_node, remote_vuln],
+            2: [source_node, port, cred],
+        }
+        for part in other_action_parts[int(sampled_type)]:
+            sampled, sampled_logp, _ = self._mask_and_sample_action_part(dists[part], subtree, deterministic)
+            action[part] = sampled
+            logp[part] = sampled_logp
+            subtree = subtree[int(sampled)]
 
-        if sampled_type == 0:
-            for part in [source_node, local_vuln]:
-                sampled = self._mask_and_sample_action_part(subtree, part, deterministic)
-                action[part] = sampled
-                subtree = subtree[int(sampled)]
-        elif sampled_type == 1:
-            for part in [source_node, target_node, remote_vuln]:
-                sampled = self._mask_and_sample_action_part(subtree, part, deterministic)
-                action[part] = sampled
-                subtree = subtree[int(sampled)]
-        else:
-            for part in [source_node, port, cred]:
-                sampled = self._mask_and_sample_action_part(subtree, part, deterministic)
-                action[part] = sampled
-                subtree = subtree[int(sampled)]
-        return action.unsqueeze(0)
+        return action, logp.sum()
 
-    def _mask_and_sample_action_part(
-        self,
-        subtree: Dict,
-        dist_pos: int,
-        deterministic: bool
-    ) -> th.Tensor:
-        options = list(subtree.keys())
-        mask = np.full_like(self.distributions[dist_pos].logits[0], False, dtype=bool)
+    @staticmethod
+    def _mask_and_sample_action_part(dist: MaskableCategorical, vat_subtree: Dict,
+                                     deterministic: bool = False) -> Tuple[th.Tensor, th.Tensor, np.ndarray]:
+        options = list(vat_subtree.keys())
+        # mask = np.full_like(dist.logits, False, dtype=bool)
+        mask = np.full(tuple(dist.logits.shape), False, dtype=bool)
         mask[options] = True
-        self.distributions[dist_pos].apply_masking(mask)
+        dist.apply_masking(mask)
 
         if deterministic:
-            sampled = th.argmax(self.distributions[dist_pos].probs)
+            sampled = th.argmax(dist.probs)
         else:
-            sampled = self.distributions[dist_pos].sample()
-        return sampled
+            sampled = dist.sample()
+        logp = dist.log_prob(sampled)
+        return sampled, logp, mask
 
-
-# class VATMultiCategoricalDistribution(VATDistribution):
-#     def __init__(self, action_dims: List[int]):
-#         super().__init__()
-#         self.distributions: List[MaskableCategorical] = []
-#         self.action_dims = action_dims
-#
-#     def proba_distribution_net(self, latent_dim: int) -> nn.Module:
-#         action_logits = nn.Linear(latent_dim, sum(self.action_dims))
-#         return action_logits
-#
-#     def proba_distribution(self, action_logits: th.Tensor) -> "VATMultiCategoricalDistribution":
-#         reshaped_logits = action_logits.view(-1, sum(self.action_dims))
-#
-#         self.distributions = [
-#             MaskableCategorical(logits=split) for split in th.split(reshaped_logits, tuple(self.action_dims), dim=1)
-#         ]
-#         return self
-#
-#     def log_prob(self, actions: th.Tensor) -> th.Tensor:
-#         assert len(self.distributions) > 0, "Must set distribution parameters"
-#
-#         # Restructure shape to align with each categorical
-#         actions = actions.view(-1, len(self.action_dims))
-#
-#         # Extract each discrete action and compute log prob for their respective distributions
-#         return th.stack(
-#             [dist.log_prob(action) for dist, action in zip(self.distributions, th.unbind(actions, dim=1))], dim=1
-#         ).sum(dim=1)
-#
-#     def entropy(self) -> th.Tensor:
-#         assert len(self.distributions) > 0, "Must set distribution parameters"
-#         return th.stack([dist.entropy() for dist in self.distributions], dim=1).sum(dim=1)
-#
-#     def sample(self, valid_action_trees: Optional[np.ndarray] = None) -> th.Tensor:
-#         assert len(self.distributions) > 0, "Must set distribution parameters"
-#         sampled, _ = self._mask_and_sample(valid_action_trees, False)
-#         return sampled
-#
-#     def mode(self, valid_action_trees: Optional[np.ndarray] = None) -> th.Tensor:
-#         assert len(self.distributions) > 0, "Must set distribution parameters"
-#         sampled, _ = self._mask_and_sample(valid_action_trees, True)
-#         return sampled
-#
-#     def get_actions(self, deterministic: bool = False, valid_action_trees: Optional[np.ndarray] = None) -> th.Tensor:
-#         if deterministic:
-#             return self.mode(valid_action_trees)
-#         return self.sample(valid_action_trees)
-#
-#     def actions_from_params(self, action_logits: th.Tensor, deterministic: bool = False) -> th.Tensor:
-#         # Update the proba distribution
-#         self.proba_distribution(action_logits)
-#         return self.get_actions(deterministic=deterministic)
-#
-#     def log_prob_from_params(self, action_logits: th.Tensor) -> Tuple[th.Tensor, th.Tensor]:
-#         actions = self.actions_from_params(action_logits)
-#         log_prob = self.log_prob(actions)
-#         return actions, log_prob
-#
-#     def apply_vat(self, vat: Optional[np.ndarray]) -> None:
-#         pass
-#
-#     def logp_entropy_for_action(self, action: th.Tensor, valid_action_tree: Optional[np.ndarray]) -> Tuple[th.Tensor, th.Tensor]:
-#         action_type, source_node, target_node, local_vuln, remote_vuln, port, cred = range(7)
-#
-#         logp_parts = th.zeros([7])
-#         entropy_parts = th.zeros([7])
-#
-#         sampled_type = action[action_type]
-#         subtree = valid_action_tree[sampled_type]
-#         if sampled_type == 0:
-#             for part in [source_node, local_vuln]:
-#                 options = list(subtree.keys())
-#                 mask = np.full_like(self.distributions[part].logits, False, dtype=bool)
-#                 mask[options] = True
-#                 self.distributions[part].apply_masking(mask)
-#                 logp_parts[part] = self.distributions[part].log_prob(action[part])
-#                 entropy_parts[part] = self.distributions[part].entropy()
-#         elif sampled_type == 1:
-#             for part in [source_node, target_node, remote_vuln]:
-#                 options = list(subtree.keys())
-#                 mask = np.full_like(self.distributions[part].logits, False, dtype=bool)
-#                 mask[options] = True
-#                 self.distributions[part].apply_masking(mask)
-#                 logp_parts[part] = self.distributions[part].log_prob(action[part])
-#                 entropy_parts[part] = self.distributions[part].entropy()
-#         elif sampled_type == 2:
-#             for part in [source_node, port, cred]:
-#                 options = list(subtree.keys())
-#                 mask = np.full_like(self.distributions[part].logits, False, dtype=bool)
-#                 mask[options] = True
-#                 self.distributions[part].apply_masking(mask)
-#                 logp_parts[part] = self.distributions[part].log_prob(action[part])
-#                 entropy_parts[part] = self.distributions[part].entropy()
-#         else:
-#             raise ValueError
-#         return logp_parts.sum(), entropy_parts.sum()
-#
-#     def _mask_and_sample(self, valid_action_tree: Optional[np.ndarray], deterministic: bool = False) -> Tuple[
-#         th.Tensor, th.Tensor]:
-#         assert len(self.distributions) > 0, "Must set distribution params"
-#         if valid_action_tree is None:
-#             if deterministic:
-#                 actions = self.mode()
-#             else:
-#                 actions = self.sample()
-#             logp = self.log_prob(actions)
-#             return actions, logp
-#         assert isinstance(valid_action_tree[0][0], Dict)
-#         vat: Dict = valid_action_tree[0][0]
-#
-#         # this is a shitshow of hardcoded values
-#         action = th.zeros([7])
-#         logp_parts = th.zeros([7])
-#
-#         action_type, source_node, target_node, local_vuln, remote_vuln, port, cred = range(7)
-#         sampled_type, sampled_type_logp = self._mask_and_sample_action_part(vat, action_type)
-#         logp_parts[action_type] = sampled_type_logp
-#         action[action_type] = sampled_type
-#         subtree = vat[sampled_type]
-#
-#         if sampled_type == 0:
-#             for part in [source_node, local_vuln]:
-#                 sampled, sampled_logp = self._mask_and_sample_action_part(part, subtree, deterministic)
-#                 logp_parts[part] = sampled_logp
-#                 action[part] = sampled
-#                 subtree = subtree[sampled]
-#         elif sampled_type == 1:
-#             for part in [source_node, target_node, remote_vuln]:
-#                 sampled, sampled_logp = self._mask_and_sample_action_part(part, subtree, deterministic)
-#                 logp_parts[part] = sampled_logp
-#                 action[part] = sampled
-#                 subtree = subtree[sampled]
-#         elif sampled_type == 2:
-#             for part in [source_node, port, cred]:
-#                 sampled, sampled_logp = self._mask_and_sample_action_part(part, subtree, deterministic)
-#                 logp_parts[part] = sampled_logp
-#                 action[part] = sampled
-#                 subtree = subtree[sampled]
-#         else:
-#             raise ValueError
-#         return action, logp_parts.sum()
-#
-#     def _mask_and_sample_action_part(
-#         self,
-#         vat_subtree: Dict,
-#         dist_pos: int, deterministic: bool
-#     ) -> Tuple[th.Tensor, th.Tensor]:
-#         options = list(vat_subtree.keys())
-#         mask = np.full_like(self.distributions[dist_pos].logits, False, dtype=bool)
-#         mask[options] = True
-#         self.distributions[dist_pos].apply_masking(mask)
-#
-#         if deterministic:
-#             sampled = th.argmax(self.distributions[dist_pos].probs)
-#         else:
-#             sampled = self.distributions[dist_pos].sample()
-#         logp = self.distributions[dist_pos].log_prob(sampled)
-#         return sampled, logp
+    @staticmethod
+    def _split_categorical(dist: MaskableCategorical) -> List[MaskableCategorical]:
+        return [MaskableCategorical(logits=logits) for logits in dist.logits]
 
 
 def make_vat_proba_distribution(action_space: spaces.Space) -> VATDistribution:
     if isinstance(action_space, spaces.MultiDiscrete):
         return VATMultiCategoricalDistribution(action_space.nvec)
     else:
-        raise NotImplementedError(
-            "Error: probability distribution, not implemented for action space"
-            f"of type {type(action_space)}."
-            " Must be of type Gym Spaces: Discrete, MultiDiscrete."
-        )
+        raise NotImplementedError
